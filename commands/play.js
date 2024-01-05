6d6470a870d42b934a0fc654d6e5c9488e3e96e9
tareklaaouane <tlaaouane@gmail.com>
2024-01-05 01:37:14 +0100

    complete play.js

--- commands/play.js
+++ commands/play.js
@@ -1,68 +1,113 @@
-const {SlashCommandBuilder} = require("@discordjs/builders");
-const {MessageEmbed} = require("discord.js");
-const {QueryType} = require("discord-player");
-
-/tmezzek playlista <mezzika>
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageEmbed } = require("discord.js")
+const { QueryType } = require("discord-player")
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("tmezzek")
-    .setDescription("tle9 chi mezzika")
-    .addSubcommand(Subcommand=>{
-      Subcommand.setName("9allab 3la mezzika")
-      .setDescription("hana kan9allab 3la mezzika ")
-      .addStringOption(Option => {
-        Option.setName("smia dial oughnia")
-        .setDescription("kat9lleb 3laaaa ")
-        .setRequired(true)
-      })
-    })
-    .addSubcommand(Subcommand=>{
-      Subcommand.setName("playlista")
-      .setDescription("hana anmezzkek mn playlist dial youtib ")
-      .addStringOption(Option => {
-        Option.setName("lien dial mzzika")
-        .setDescription("lien dial playlist ")
-        .setRequired(true)
-      })
-    })
-    .addSubcommand(Subcommand=>{
-      Subcommand.setName("mezzika")
-      .setDescription("hana kan9allab 3la mezzika ")
-      .addStringOption(Option => {
-        Option.setName("lien dial mzzika")
-        .setDescription("lien dial playlist ")
-        .setRequired(true)
-      })
-    }),
-    execute : async ({client,interaction})=>{
-      if (!interaction.member.voice.channel) {
-        await interaction.reply("khass tkoun f chi voice channel bash tmezzek a sat");
-        return;
-      }
-      const queue=await client.player.createQueue(interaction.guild);
-      if(!queue.connection)await queue.connect(interation.member.voice.channel)
+	data: new SlashCommandBuilder()
+		.setName("tmezzek")
+		.setDescription("kheddem mezzika")
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName("9lleb")
+				.setDescription("ana kan9allab 3la mezzika tsenna")
+				.addStringOption(option =>
+					option.setName("searchterms").setDescription("search keywords").setRequired(true)
+				)
+		)
+        .addSubcommand(subcommand =>
+			subcommand
+				.setName("playlist")
+				.setDescription("Plays a playlist from YT")
+				.addStringOption(option => option.setName("url").setDescription("the playlist's url").setRequired(true))
+		)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName("song")
+				.setDescription("Plays a single song from YT")
+				.addStringOption(option => option.setName("url").setDescription("the song's url").setRequired(true))
+		),
+	execute: async ({ client, interaction }) => {
+        // Make sure the user is inside a voice channel
+		if (!interaction.member.voice.channel) return interaction.reply("You need to be in a Voice Channel to play a song.");
 
-      let embed = new MessageEmbed();
-      if (interaction.options.getSubcommand() === "mezzika")
-      {
-        let url = interaction.options.getString("url");
-        const result = await client.player.search(url,{
-          requestedBy:interaction.user,
-          searchEngine:QueryType.AUTO,
+        // Create a play queue for the server
+		const queue = await client.player.createQueue(interaction.guild);
 
-        });
-        if(result.tracks.length === 0){
-          await interation.replay("makayna hta oughnia bhal hakka awjeh tobba");
-          return
-        }
+        // Wait until you are connected to the channel
+		if (!queue.connection) await queue.connect(interaction.member.voice.channel)
 
-        const song = result.tracks[0]
-        await queue.addTrack(song);
-        embed.setDescription(`rah tzadet **[${song.title}](${song.url})** lqueue`)
-        .setThumbnail(song.thumbnail)
-        .setFooter({text:`chhal fih: ${song.duration}`});
-        }
-      }
-    }
-    
+		let embed = new MessageEmbed()
+
+		if (interaction.options.getSubcommand() === "song") {
+            let url = interaction.options.getString("url")
+            
+            // Search for the song using the discord-player
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_VIDEO
+            })
+
+            // finish if no tracks were found
+            if (result.tracks.length === 0)
+                return interaction.reply("No results")
+
+            // Add the track to the queue
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}`})
+
+		}
+        else if (interaction.options.getSubcommand() === "playlist") {
+
+            // Search for the playlist using the discord-player
+            let url = interaction.options.getString("url")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_PLAYLIST
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.reply(`No playlists found with ${url}`)
+            
+            // Add the tracks to the queue
+            const playlist = result.playlist
+            await queue.addTracks(result.tracks)
+            embed
+                .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** have been added to the Queue`)
+                .setThumbnail(playlist.thumbnail)
+
+		} 
+        else if (interaction.options.getSubcommand() === "search") {
+
+            // Search for the song using the discord-player
+            let url = interaction.options.getString("searchterms")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.AUTO
+            })
+
+            // finish if no tracks were found
+            if (result.tracks.length === 0)
+                return interaction.editReply("No results")
+            
+            // Add the track to the queue
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}`})
+		}
+
+        // Play the song
+        if (!queue.playing) await queue.play()
+        
+        // Respond with the embed containing information about the player
+        await interaction.reply({
+            embeds: [embed]
+        })
+	},
+}
